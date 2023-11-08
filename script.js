ea0a3c4c1e3a9d0b932018a2164496b5efeca055
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-08 23:31:18 +0530

    Added Note Edit Feature

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const NOTES = [
-  {
-    bg: "#14a44d",
-    description:
-      "my name is saiful from bijulia thakurbari chakulia uttar dinajpur west bengal",
-    time: "09:45:03 AM",
-  },
+  // {
+  //   bg: "#14a44d",
+  //   description:
+  //     "my name is saiful from bijulia thakurbari chakulia uttar dinajpur west bengal",
+  //   time: "09:45:03 AM",
+  // },
 ];
 
 // Input Area Element
@@ -23,16 +23,12 @@ function updateNotesUi() {
             style="background-color: ${note.bg}"
           >
             <div class="actionBtns w-full flex justify-end gap-3">
-              <i id="${index}" class="editBtn fa-regular fa-pen-to-square"></i>
-              <i
-                id="${index}"
-                class="deleteBtn cursor-pointer text-red-600 fa-solid fa-trash-can"
+              <i  class="sendToEditBtn fa-regular fa-pen-to-square"></i>
+              <i  class="deleteBtn cursor-pointer text-red-600 fa-solid fa-trash-can"
               ></i>
             </div>
             <div id="des" class="description mt-2">${note.description}</div>
-            <span id="time" class="time absolute bottom-2 right-2 text-[.6rem]">
-              ${note.time}</span
-            >
+            <span id="time" class="time absolute bottom-1.5 right-2 text-[.6rem] tracking-wider">${note.time}</span>
           </div>`;
   });
 
@@ -40,8 +36,8 @@ function updateNotesUi() {
 }
 updateNotesUi();
 function addNote() {
-  let textAreaInput = document.getElementById("text_area_input").value;
-  const colorInput = document.getElementById("color_input").value;
+  let textAreaInput = document.getElementById("text_area_input");
+  const colorInput = document.getElementById("color_input");
   const currentTime = new Date();
 
   // Format the current time
@@ -53,26 +49,65 @@ function addNote() {
   });
 
   let noteData = {};
-  noteData.description = textAreaInput;
-  noteData.bg = colorInput;
+  noteData.description = textAreaInput.value;
+  noteData.bg = colorInput.value;
   noteData.time = formattedTime;
 
   NOTES.push(noteData);
   updateNotesUi();
-  console.log(NOTES);
-
-  textAreaInput = "";
+  textAreaInput.value = "";
 }
 
 addBtn.addEventListener("click", addNote);
 
 // Delete note
-function deleteNote(e) {
-  if (e.target.classList.contains("deleteBtn")) {
-    const index = e.target.id;
-    NOTES.splice(index, 1);
-    updateNotesUi();
+notesWrapper.addEventListener("click", noteActions);
+
+function noteActions(e) {
+  const index = e.target.parentElement.parentElement.id;
+  if (e.target.classList.contains("sendToEditBtn")) {
+    sendToEditBtnfn(index);
+  } else if (e.target.classList.contains("deleteBtn")) {
+    deleteNote(index)
   }
 }
+function deleteNote(index) {
+    NOTES.splice(index, 1);
+    updateNotesUi();
+}
 
-notesWrapper.addEventListener("click", deleteNote);
+
+function sendToEditBtnfn(index) {
+  let textAreaInput = document.getElementById("text_area_input");
+  let colorInput = document.getElementById("color_input");
+
+  textAreaInput.value = NOTES[index].description;
+  colorInput.value = NOTES[index].bg;
+  textAreaInput.setAttribute("editIndex", index);
+  addBtn.style.display = "none";
+  editBtn.style.display = "block";
+
+}
+
+const editBtn = document.getElementById("editBtn");
+
+editBtn.addEventListener("click", editNote);
+
+function editNote() {
+  let textAreaInput = document.getElementById("text_area_input");
+  let colorInput = document.getElementById("color_input");
+
+  const index = textAreaInput.getAttribute("editindex");
+
+  NOTES[index].description = textAreaInput.value;
+  NOTES[index].bg = colorInput.value;
+  NOTES[index].time = "Edited " + NOTES[index].time;
+  updateNotesUi();
+
+  textAreaInput.value = "";
+  colorInput.value = "";
+
+    addBtn.style.display="block";
+    editBtn.style.display="none";
+}
+
