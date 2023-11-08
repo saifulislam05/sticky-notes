ac0152474196eff839e95707fc366dce38a20781
saifulislam05 <saiful.bijulia@gmail.com>
2023-11-08 09:48:32 +0530

    project done v1

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const NOTES = [];
+const NOTES = [
+  {
+    bg: "#14a44d",
+    description:
+      "my name is saiful from bijulia thakurbari chakulia uttar dinajpur west bengal",
+    time: "09:45:03 AM",
+  },
+];
 
 // Input Area Element
 const addBtn = document.getElementById("addBtn");
@@ -7,43 +14,65 @@ const addBtn = document.getElementById("addBtn");
 const notesWrapper = document.getElementById("notes_wrapper");
 const deleteBtn = document.querySelector(".deleteBtn");
 
-
 function updateNotesUi() {
-    let noteItems = "";
-    NOTES.forEach((note, index) => {
-        noteItems += `<div id=${index} class="note text-white rounded-xl p-4 relative" style="background-color:${note.bg} ;">
-                    <i id=${index}
-                            class="deleteBtn absolute right-2 top-2 cursor-pointer text-red-600 fa-solid fa-trash-can"></i>
-                    <div id="des" class="description">${note.description}</div>
-                </div>`;
-    });
+  let noteItems = "";
+  NOTES.forEach((note, index) => {
+    noteItems += `<div
+            id="${index}"
+            class="note text-white rounded-xl p-4 relative"
+            style="background-color: green"
+          >
+            <div class="actionBtns w-full flex justify-end gap-3">
+              <i id="${index}" class="editBtn fa-regular fa-pen-to-square"></i>
+              <i
+                id="${index}"
+                class="deleteBtn cursor-pointer text-red-600 fa-solid fa-trash-can"
+              ></i>
+            </div>
+            <div id="des" class="description mt-2">${note.description}</div>
+            <span id="time" class="time absolute bottom-2 right-2 text-[.6rem]">
+              ${note.time}</span
+            >
+          </div>`;
+  });
 
-    notesWrapper.innerHTML = noteItems;
-
+  notesWrapper.innerHTML = noteItems;
 }
 updateNotesUi();
 function addNote() {
-    let textAreaInput = document.getElementById("text_area_input").value;
-    const colorInput = document.getElementById("color_input").value;
-    let noteData = {};
-    noteData.description = textAreaInput;
-    noteData.bg = colorInput;
-    NOTES.push(noteData);
-    updateNotesUi();
+  let textAreaInput = document.getElementById("text_area_input").value;
+  const colorInput = document.getElementById("color_input").value;
+  const currentTime = new Date();
 
-    textAreaInput = "";
+  // Format the current time
+  const formattedTime = currentTime.toLocaleTimeString(undefined, {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  let noteData = {};
+  noteData.description = textAreaInput;
+  noteData.bg = colorInput;
+  noteData.time = formattedTime;
+
+  NOTES.push(noteData);
+  updateNotesUi();
+  console.log(NOTES);
+
+  textAreaInput = "";
 }
 
 addBtn.addEventListener("click", addNote);
 
-// Delete note 
+// Delete note
 function deleteNote(e) {
-    if (e.target.classList.contains("deleteBtn")) {
-        const index = e.target.id;
-        NOTES.splice(index, 1)
-        updateNotesUi();
-    }
-
+  if (e.target.classList.contains("deleteBtn")) {
+    const index = e.target.id;
+    NOTES.splice(index, 1);
+    updateNotesUi();
+  }
 }
 
 notesWrapper.addEventListener("click", deleteNote);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".html"],
+  content: ["*.html"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
   plugins: [require("daisyui")],
 };
